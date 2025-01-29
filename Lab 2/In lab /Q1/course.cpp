d12c508f85a5414468552c3516e587946f04d777
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-01-29 22:59:53 +0500

    Update course.cpp

--- Lab 2/In lab /Q1/course.cpp
+++ Lab 2/In lab /Q1/course.cpp
@@ -4,31 +4,38 @@ using namespace std;
 
 Course::Course(const string& name) : name(name) {}
 
-void Course::enroll_Student(Student* student) {
+void Course::enroll_Student(Student* student) 
+{
     students.push_back(student);
     student->enroll(this);
 }
 
-void Course::assign_Teacher(Teacher* teacher) {
+void Course::assign_Teacher(Teacher* teacher) 
+{
     teachers.push_back(teacher);
     teacher->assign_Course(this);
 }
 
-void Course::display_Students() const {
+void Course::display_Students() const 
+{
     cout << "Course: " << name << " has the following students enrolled:\n";
-    for (const auto& student : students) {
+    for (const auto& student : students) 
+    {
         cout << "- " << student->getName() << "\n";
     }
 }
 
-void Course::display_Teachers() const {
+void Course::display_Teachers() const 
+{
     cout << "Course: " << name << " is taught by:\n";
-    for (const auto& teacher : teachers) {
+    for (const auto& teacher : teachers) 
+    {
         cout << "- " << teacher->getName() << "\n";
     }
 }
 
-string Course::getName() const {
+string Course::getName() const 
+{
     return name;
 }
 
