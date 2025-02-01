3d286d1decf8e0547163b9f74fd06df271e5beb9
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-02-01 23:38:44 +0500

    Update and rename Lab 2/In lab /Q2.cpp to Lab 2/In lab /Q2/Q2.cpp

---
+++ Lab 2/In lab /Q2/Q2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " <filename>\n";
+        return 1;
+    }
+
+    ifstream file(argv[1]);
+    if (!file) {
+        cout << "Error: File not found.\n";
+        return 1;
+    }
+
+    char ch;
+    while (file.get(ch)) cout << ch; // Print file contents character by character
+
+    return 0;
+}
