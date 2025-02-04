4e2843b47f227d8f5733d7efef4a7e467eaee16a
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-02-04 22:37:23 +0500

    Update LetterCount.h

--- Lab 2/Post Lab/Q4/LetterCount.h
+++ Lab 2/Post Lab/Q4/LetterCount.h
@@ -1,32 +1,12 @@
 #ifndef LETTERCOUNT_H
 #define LETTERCOUNT_H
 
-#include <fstream>
-#include <cctype>
+#include <string>
 using namespace std;
-
 class LetterCount
 {
 public:
-    int countLetters(const string &filename)
-    {
-        ifstream file(filename);
-        if (!file)
-        {
-            return -1;
-        }
-
-        char ch;
-        int count = 0;
-        while (file.get(ch))
-        {
-            if (isalpha(ch))
-            {
-                count++;
-            }
-            return count;
-        }
-    }
+    int countLetters(const string &filename);
 };
 
 #endif
