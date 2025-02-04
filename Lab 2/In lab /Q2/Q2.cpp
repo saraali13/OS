c7a588faa81e33502e4e3af7755553e1fbd72e2b
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-02-04 18:24:05 +0500

    Update Q2.cpp

--- Lab 2/In lab /Q2/Q2.cpp
+++ Lab 2/In lab /Q2/Q2.cpp
@@ -2,23 +2,27 @@
 #include <fstream>
 using namespace std;
 
-int main(int argc, char* argv[]) 
+int main(int argc, char *argv[])
 {
-    if (argc != 2) 
+    if (argc != 2)
     {
         cout << "Usage: " << argv[0] << " <filename>\n";
         return 1;
     }
 
     ifstream file(argv[1]);
-    if (!file) 
+
+    if (!file)
     {
         cout << "Error: File not found.\n";
         return 1;
     }
 
     char ch;
-    while (file.get(ch)) cout << ch; // Print file contents character by character
+    while (file.get(ch))
+    {
+        cout << ch; // Print file contents character by character
+    }
 
     return 0;
 }
