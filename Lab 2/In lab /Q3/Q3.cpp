ba4c92ff465be7c9d0f81198923e74b9d2d8a0e4
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-02-04 18:23:41 +0500

    Update Q3.cpp

--- Lab 2/In lab /Q3/Q3.cpp
+++ Lab 2/In lab /Q3/Q3.cpp
@@ -2,20 +2,27 @@
 #include <algorithm>
 using namespace std;
 
-int main(int argc, char* argv[]) 
+int main(int argc, char *argv[])
 {
-    if (argc < 2) 
+    if (argc < 2)
     {
         cout << "Usage: " << argv[0] << " <list of integers>\n";
         return 1;
     }
 
     int arr[argc - 1];
-    for (int i = 1; i < argc; i++) arr[i - 1] = atoi(argv[i]);
+    for (int i = 0; i < argc - 1; i++)
+    {
+        arr[i] = atoi(argv[i+1]);
+    }
 
-    sort(arr, arr + argc - 1);
+    sort(arr, arr + (argc - 1));
 
-    for (int i = 0; i < argc - 1; i++) cout << arr[i] << " ";
+    for (int i = 0; i < argc - 1; i++)
+    {
+        cout << arr[i] << ", ";
+    }
+
     cout << endl;
 
     return 0;
