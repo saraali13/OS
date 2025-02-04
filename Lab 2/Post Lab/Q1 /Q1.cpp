6d2421f048658d4dc5ceaee57105f06c0a081a81
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-02-04 18:58:12 +0500

    Update Q1.cpp

--- Lab 2/Post Lab/Q1 /Q1.cpp
+++ Lab 2/Post Lab/Q1 /Q1.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
-#include <cstdlib> // For atoi()
+#include <cstdlib>
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        cout << "Usage: " << argv[0] << " <integers>\n";
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        cout << "Usage: " << argv[0] << " <integers\n>";
         return 1;
     }
-
+    
     int sum = 0;
-    for (int i = 1; i < argc; i++) {
-        sum += atoi(argv[i]);
+    for (int i = 0; i < argc - 1; i++)
+    {
+        sum += atoi(argv[i + 1]);
     }
 
-    double avg = (double)sum / (argc - 1);
-    cout << "Sum: " << sum << "\nAverage: " << avg << endl;
-
+    float avg = (float)sum / (argc - 1);
+    cout << "Sum: " << sum << endl;
+    cout << "Average: " << avg << endl;
     return 0;
 }
