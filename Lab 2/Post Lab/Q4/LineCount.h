e0c1586c8f49f1c3b2deccbd01271afceb232ba8
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-02-04 22:41:21 +0500

    Update LineCount.h

--- Lab 2/Post Lab/Q4/LineCount.h
+++ Lab 2/Post Lab/Q4/LineCount.h
@@ -1,29 +1,13 @@
 #ifndef LINECOUNT_H
 #define LINECOUNT_H
 
-#include <fstream>
 #include <string>
 using namespace std;
 
 class LineCount
 {
 public:
-    static int countLines(const string &filename)
-    {
-        ifstream file(filename);
-        if (!file)
-        {
-            return -1;
-        }
-
-        string line;
-        int count = 0;
-        while (getline(file, line))
-        {
-            count++;
-        }
-        return count;
-    }
+    int countLines(const string &filename);
 };
 
 #endif
