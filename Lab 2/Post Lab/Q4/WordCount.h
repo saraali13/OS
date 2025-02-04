da8789e9652c09a41bfb465f194e9b416f2bb778
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-02-04 22:39:35 +0500

    Update WordCount.h

--- Lab 2/Post Lab/Q4/WordCount.h
+++ Lab 2/Post Lab/Q4/WordCount.h
@@ -1,29 +1,13 @@
 #ifndef WORDCOUNT_H
 #define WORDCOUNT_H
 
-#include <fstream>
 #include <string>
 using namespace std;
 
 class WordCount
 {
 public:
-    int countWords(const string &filename)
-    {
-        ifstream file(filename);
-        if (!file)
-        {
-            return -1;
-        }
-
-        string word;
-        int count = 0;
-        while (file >> word)
-        {
-            count++;
-        }
-        return count;
-    }
+    int countWords(const string &filename);
 };
 
 #endif
